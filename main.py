ed0163e93d3ac3850f2a5943363a1826ad316c6d
Daivik_Suneesh_Kumar <daivik.suneesh@gmail.com>
2026-06-03 17:22:08 +0530

    Logical Operators

---
+++ main.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All numbers have boolean value.")
+else:
+    print("No number has boolean value.")
+
+a = 10
+b = -12
+c = 1
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if b > 0 or c > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
